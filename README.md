e47f819e25ffa9ce003c3b73f80c272b1803fc42
Gonzalo Sempol <gonzalo.sempol@gmail.com>
2022-11-26 18:26:47 -0300

    Clase grafo con metodos para insertar nodos y aristas ✅

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# obligatorio2-teoriaComp
+# obligatorio2-teoriaComp 
  
+ **Documento**
+-  [TODO: Documento entregable](https://TODO:)
+
+**Consideraciones**
+Los nodos deben ser objetos de la forma:
+	
+
+    {
+      'key':'valorDeClaveUnico', 
+      'atrib1':'valor1',
+      ... 
+      'atribN':'valorN'
+     }
+
+ **Requerimientos:**
+ - python3.6+✅
+
+ **Para correr el codigo:** 
+ - `py main.py`
--- main.py
+++ main.py
@@ -1,55 +1,66 @@
 class nodo:
-    nodo = {} 
-    adyacencias = set() #set de nodos, O(1) de insercion y busqueda
-
     def __init__(self, nodo): #un nodo puede ser cualquier objeto, pero debe tener un atributo key enumerable
         self.nodo = nodo;
+        self.adyacencias = set() #set de nodos, O(1) de insercion y busqueda
+    
     def getKey(self): #El objeto debe tener un atributo key
         return self.nodo.get('key')
+    
     def insertarAdyacencia(self, nodoDest):
         self.adyacencias.add(nodoDest)
+    
     def getAdyacencias(self):
         return self.adyacencias
-
+    
+    def printAdyacenciasByKey(self):     
+        adybykey = map(lambda x: (x.getKey()), (self.adyacencias))
+        list(map(print,adybykey))
 
 class grafo:
-    grafo = {} #O(1) de insercion y busqueda
+    def __init__(self): 
+        self.grafo = {} #O(1) de insercion y busqueda
+    
     def insertarNodo(self,nodo):
-        self.grafo.update({nodo.getKey():nodo})
+        self.grafo[nodo.getKey()] = nodo
+    
     def getNodoByKey(self, keyNodo):
         return self.grafo.get(keyNodo)
+    
     def insertarArista(self, keyNodoOrigen, keyNodoDestino):
         nodoOrigen = (self.getNodoByKey(keyNodoOrigen))
-        print('nodo origen')
-        print(nodoOrigen)
         nodoDestino = (self.getNodoByKey(keyNodoDestino))
-        print('nodo destino')
-        print(nodoDestino)
         nodoOrigen.insertarAdyacencia(nodoDestino)
         nodoDestino.insertarAdyacencia(nodoOrigen)
+        
 
     
 
 miGrafo = grafo()
-nodo1 = nodo({'objetito': 'algo', 'key':'pepe'})
-nodo2 = nodo({'objetito': 'otra cosa', 'key':'pepe2'})
-nodo3 = nodo({'objetito': 'otra cosa 2', 'key':'pepe3'})
+nodo1 = nodo({'objetito': 'algo', 'key':'nodo1'})
+nodo2 = nodo({'objetito': 'otra cosa', 'key':'nodo2'})
+nodo3 = nodo({'objetito': 'otra cosa 2', 'key':'nodo3'})
 
 
 miGrafo.insertarNodo(nodo1)
 miGrafo.insertarNodo(nodo2)
 miGrafo.insertarNodo(nodo3)
-miGrafo.insertarArista('pepe','pepe2')
-miGrafo.insertarArista(nodo2.getKey(),nodo3.getKey())
+miGrafo.insertarArista('nodo1','nodo2')
+miGrafo.insertarArista('nodo2','nodo3')
 
 print('GRAFO:')
 print(miGrafo.grafo)
-print((miGrafo.getNodoByKey('pepe')).getKey())
-print((miGrafo.getNodoByKey('pepe2')).getKey())
+print('pruebo traer nodo1 by key')
+print((miGrafo.getNodoByKey('nodo1')).getKey())
+print('pruebo traer nodo2 by key')
+print((miGrafo.getNodoByKey('nodo2')).getKey())
+print('ady nodo1:')
+nodo1.printAdyacenciasByKey()
+print('ady nodo2:')
+nodo2.printAdyacenciasByKey()
+print('ady nodo3:')
+nodo3.printAdyacenciasByKey()
 
-adyacencias = miGrafo.getNodoByKey('pepe').getAdyacencias()
-print(adyacencias)
-adybykey = map(lambda x: (x.getKey()) ,(adyacencias))
-print('ady de pepe')
-list(map(print,adybykey))
+
+
+
 
