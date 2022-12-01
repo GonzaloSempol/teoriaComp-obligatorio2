c662ff1618df5b4aa38a8cc4690267af320715b9
Gonzalo Sempol <gonzalo.sempol@gmail.com>
2022-12-01 20:02:50 -0300

    Se agrega bfs y override del print del nodo

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ class nodo:
         self.adyacencias = set() #set de nodos, O(1) de insercion y busqueda
         self.visitado=False #Atributo que se utiliza en algoritmos para controlar si un nodo fue visitado
     
+    #Redefinimos el print para que muestre la clave al imprimir un nodo
+    def __repr__(self):
+        return self.getKey()     
+
     def getKey(self): #El objeto debe tener un atributo key
         return self.nodo.get('key')
     
@@ -44,9 +48,7 @@ class grafo:
     def setearNodosSinVisitar(self):
         for key in self.grafo : #Para todas las keys
             self.getNodoByKey(key).setNoVisitado() #Obtengo el nodo y lo seteo sin visitar
-    def printCamino(self, nodos): #Recibe un array de nodos
-        for n in nodos : #Para todas las keys
-            print(n.getKey())
+
     
     #Implementación de dfs iterativa
     def dfs(self, keyNodoV): 
@@ -67,12 +69,31 @@ class grafo:
         
         return resultado
 
+    #Implementación de dfs iterativa
+    def bfs(self, keyNodoV): 
+        resultado = []
+        vInicial=self.getNodoByKey(keyNodoV); #Buscamos el nodo inicial       
+        if vInicial is not None : #Si existe el nodo inicial
+            self.setearNodosSinVisitar(); #Seteamos todos los nodos como sin visitar
+            queue = []
+            queue.append(vInicial)
+            while(queue != []):
+                v=queue.pop(0)
+                if not v.esVisitado() :
+                    v.setVisitado()
+                    resultado.append(v)
+                for vAdy in (v.getAdyacencias()) :
+                    if not vAdy.esVisitado() :
+                        queue.append(vAdy) #insertamos al final
+        
+        return resultado
         
 
     
 
 miGrafo = grafo()
 nodo0 = nodo({'objetito': 'algo', 'key':'0'})
+print(nodo0)
 nodo1 = nodo({'objetito': 'algo', 'key':'1'})
 nodo2 = nodo({'objetito': 'otra cosa', 'key':'2'})
 nodo3 = nodo({'objetito': 'otra cosa 2', 'key':'3'})
@@ -108,7 +129,7 @@ miGrafo.insertarArista('9','4')
 miGrafo.insertarArista('4','3')
 
 print('GRAFO:')
-#print(miGrafo.grafo)
+print(miGrafo.grafo)
 print('pruebo traer nodo1 by key')
 print((miGrafo.getNodoByKey('1')).getKey())
 print('pruebo traer nodo2 by key')
@@ -120,12 +141,14 @@ nodo2.printAdyacenciasByKey()
 print('ady nodo3:')
 nodo3.printAdyacenciasByKey()
 
-print('Camino:')
-result = miGrafo.dfs('2')
-miGrafo.printCamino(result)
-print('Camino dfs 0:')
-result = miGrafo.dfs('04')
-miGrafo.printCamino(result)
+print('Camino dfs 2:')
+print(miGrafo.dfs('2'))
+print('Camino dfs 04:')
+print(miGrafo.dfs('04'))
+print('Camino bfs 2:')
+print(miGrafo.bfs('2'))
+print('Camino bfs 04:')
+print(miGrafo.bfs('04'))
 
 
 
